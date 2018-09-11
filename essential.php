8a3b6f4fe364dda716352c945c34fecba8a553ce
Obaydullah <obaydullah.2041@gmail.com>
2018-09-11 12:52:55 +0600

    Dynamic title tag PHP
    This is used in titile tag.

---
+++ essential.php
@@ -0,0 +1,14 @@
+<title> 
+<?php
+	$site_description = get_bloginfo( 'description', 'display' );
+	$site_name = get_bloginfo( 'name' );
+    //for home page
+	if ( $site_description && ( is_home() || is_front_page() ) ):
+		echo $site_name;echo ' | '; echo  $site_description; 
+	endif;
+	// for other post pages
+	if (!( is_home() ) && ! is_404() ):
+	the_title(); echo ' | '; echo $site_name;
+	endif;
+	?>
+</title>
